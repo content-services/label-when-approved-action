0d62f66445f0369bd0809d19275ab9cb92f6a5f4
Justin Sherrill <jsherril@redhat.com>
2023-02-02 09:45:38 -0500

    lots of fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,46 +36,41 @@ state=$(jq --raw-output .review.state "$GITHUB_EVENT_PATH")
 number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 
 label_when_approved() {
-  teamMembers=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "https://api.github.com/organizations/102682140/team/7316832/members" | jq ".[].login") #TODO make the team name configurable
+  teamMembers=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "https://api.github.com/organizations/102682140/team/7316832/members" | jq --raw-output ".[].login") #TODO make the team name configurable
 
   # https://developer.github.com/v3/pulls/reviews/#list-reviews-on-a-pull-request
   body=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/${GITHUB_REPOSITORY}/pulls/${number}/reviews?per_page=100")
-  reviews=$(echo "$body" | jq --raw-output '.[] | {state: .state, login: .login} | @base64')
+  reviews=$(echo "$body" | jq --raw-output '.[] | {state: .state, login: .user.login} | @base64')
   
   for r in $reviews; do
     review="$(echo "$r" | base64 -d)"
     rState=$(echo "$review" | jq --raw-output '.state')
     reviewer=$(echo "$review" | jq --raw-output '.login')
     if [[ "$rState" == "APPROVED" ]]; then
-      approvals=$((approvals+1))
-    fi
-    for tm in $team_members; do 
-      if [[ "$tm" == "$reviewer" ]]; then
-        echo "Labeling pull request"
+      for tm in $teamMembers; do
+        if [[ "$tm" == "$reviewer" ]]; then
+          echo "Labeling pull request"
 
-        curl -sSL \
-          -H "${AUTH_HEADER}" \
-          -H "${API_HEADER}" \
-          -X POST \
-          -H "Content-Type: application/json" \
-          -d "{\"labels\":[\"${addLabel}\"]}" \
-          "${URI}/repos/${GITHUB_REPOSITORY}/issues/${number}/labels"
+          curl -sSL \
+            -H "${AUTH_HEADER}" \
+            -H "${API_HEADER}" \
+            -X POST \
+            -H "Content-Type: application/json" \
+            -d "{\"labels\":[\"${addLabel}\"]}" \
+            "${URI}/repos/${GITHUB_REPOSITORY}/issues/${number}/labels"
 
-        if [[ -n "$REMOVE_LABEL" ]]; then
-            curl -sSL \
-              -H "${AUTH_HEADER}" \
-              -H "${API_HEADER}" \
-              -X DELETE \
-              "${URI}/repos/${GITHUB_REPOSITORY}/issues/${number}/labels/${REMOVE_LABEL}"
+          if [[ -n "$REMOVE_LABEL" ]]; then
+              curl -sSL \
+                -H "${AUTH_HEADER}" \
+                -H "${API_HEADER}" \
+                -X DELETE \
+                "${URI}/repos/${GITHUB_REPOSITORY}/issues/${number}/labels/${REMOVE_LABEL}"
+          fi
+          break
         fi
-        break
-      fi
-    done
+      done
+    fi
   done
 }
 
-if [[ "$action" == "submitted" ]] && [[ "$state" == "approved" ]]; then
-  label_when_approved
-else
-  echo "Ignoring event ${action}/${state}"
-fi
+label_when_approved
