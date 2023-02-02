bfc3c82f971084e624b9700e4805c4720bda21b7
Justin Sherrill <jsherril@redhat.com>
2023-02-02 15:29:47 -0500

    switch to org read token

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,18 +27,23 @@ if [[ -z "$addLabel" ]]; then
   exit 1
 fi
 
+if [[ -z "GH_ORG_READ_TOKEN" ]]; then
+  echo "Set the GH_ORG_READ_TOKEN env variable."
+  exit 1
+fi
 
 URI="https://api.github.com"
 API_HEADER="Accept: application/vnd.github.v3+json"
 AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
+ORG_HEADER="Authorization: token ${GH_ORG_READ_TOKEN}"
 
 action=$(jq --raw-output .action "$GITHUB_EVENT_PATH")
 state=$(jq --raw-output .review.state "$GITHUB_EVENT_PATH")
 number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 
 label_when_approved() {
-  curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "https://api.github.com/organizations/102682140/team/7323464"
-  teamMembers=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "https://api.github.com/organizations/102682140/team/7323464" | jq --raw-output ".[].login") #TODO make the team name configurable
+  curl -sSL -H "${ORG_HEADER}" -H "${API_HEADER}" "https://api.github.com/organizations/102682140/team/7323464"
+  teamMembers=$(curl -sSL -H "${ORG_HEADER}" -H "${API_HEADER}" "https://api.github.com/organizations/102682140/team/7323464" | jq --raw-output ".[].login") #TODO make the team name configurable
 
   # https://developer.github.com/v3/pulls/reviews/#list-reviews-on-a-pull-request
   body=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/${GITHUB_REPOSITORY}/pulls/${number}/reviews?per_page=100")
